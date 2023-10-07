49b38675da3a32b6e15d0a1f78a4f60e2cd2db4e
Andrey Lobutev <andrey.lobutev@gmail.com>
2023-10-07 23:37:56 +0300

    First version

--- BotMain.py
+++ BotMain.py
@@ -1,14 +1,22 @@
 import asyncio
 import logging
-import sys
-from os import getenv
 
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 
-BOT_TOKEN = 5650181298:AAEojGlUYsOqPY4wBDUTvtYxGFqZOGU2P0A
+import config
+from handlers import router
 
-TOKEN = getenv("BOT_TOKEN")
+
+async def main():
+    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
