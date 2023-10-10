c3ce98de78f2686629392fe396e377de728eb69c
Andrey Lobutev <andrey.lobutev@gmail.com>
2023-10-10 15:46:36 +0300

    Update 0.1.2

--- BotMain.py
+++ BotMain.py
@@ -4,16 +4,18 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-import config
+from config import config
 from handlers import router
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=config.BOT_TOKEN)
+bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 
 dp = Dispatcher()
 
 dp.include_router(router)
+
+mylist = [1, 2, 3]
 
 
 async def main():
--- handlers.py
+++ handlers.py
@@ -1,19 +1,44 @@
-from aiogram import types, F, Router
-from aiogram.types import Message
+from aiogram import types, F, Router, types
 from aiogram.filters import Command
+from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.filters import CommandObject
+from aiogram import html
 
 router = Router()
 
+mylist = [1, 2, 3]
+
 
 @router.message(Command("start"))
-async def cmd_start(msg: Message):
-    await msg.answer("Hello!")
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
 
 
 @router.message(Command("test1"))
-async def cmd_test1(msg: Message):
-    await msg.reply("Test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test1")
 
-@router.message(Command("Roll"))
-async def cmd_dice(msg: Message):
-    await msg.answer_dice(emoji="🎲")
+
+@router.message(Command("Roll1"))
+async def cmd_dice1(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+@router.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.reply("Добавлено число семь!")
+
+
+@router.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
+
+@router.message(Command("name"))
+async def cmd_name(message: types.Message, command: CommandObject):
+    if command.args:
+        await message.answer(f"Привет, {html.bold(html.quote(command.args))}")
+
+    else:
+        await message.answer("Пожалуйста, укажи своё имя после команды /name!")
